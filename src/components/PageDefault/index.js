2b31efff5b58069aac4e5455e54c0dd48bd1725b
Shumax <maxcastro.mv@gmail.com>
2020-07-29 21:18:52 -0300

    Formulario Categoria

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -4,7 +4,7 @@ import Footer from '../Footer';
 import styled from 'styled-components';
 
 const Main = styled.main`
-  background-color: var(--black);
+  background-color: #282c34;
   color: var(--white);
 	flex: 1;
 	height: 100vh;
--- src/pages/Registration/Category/index.js
+++ src/pages/Registration/Category/index.js
@@ -1,24 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import PageDefault from '../../../components/PageDefault';
 
 export default function RegistrationCategory() {
+	const initValuesCategory = {
+		name: '',
+		description: '',
+		color: ''
+	}
+	
+	const [valuesCategory, setvaluesCategory] = useState(initValuesCategory);
+	const [categorys, setCategorys] = useState([]);
+
+	function setValue(index, value){
+		setvaluesCategory({
+			...valuesCategory,
+			[index]: value
+		})
+	}
+
+	function handleChange(event){
+		setValue(
+			event.target.getAttribute('name'),
+			event.target.value
+		);
+		
+	}
+	
+
 	return (
 		<div>
 			<PageDefault>
-				<h1>Cadastro de Categoria</h1>
 
-				<form>
+				<h1>Cadastro de Categoria: { valuesCategory.name }</h1>
+
+				<form onSubmit={function handleSubmit(infoEvents) {
+					infoEvents.preventDefault();
+					setCategorys([
+						...categorys,
+						valuesCategory
+					]);
+					setvaluesCategory(initValuesCategory);
+				}}>
 					<label>
-						Nome da Categoria:
-						<input type="text" />
+						Título da Categoria:
+						<input 
+							type="text" 
+							value={valuesCategory.name}
+							name="name"
+							onChange={handleChange}
+						/>
 					</label>
-				
+					<label>
+						Descrição:
+						<input 
+							type="text" 
+							value={valuesCategory.description}
+							name="description"
+							onChange={handleChange}
+						/>
+					</label>
+					<label>
+						Cor:
+						<input 
+							type="color" 
+							value={valuesCategory.color}
+							name="color"
+							onChange={handleChange}
+						/>
+					</label>
+					
 					<button>
 						Cadastrar
 					</button>
+					
 				</form>
+
+				<ul>
+					{categorys.map((category, index) => {
+						return (
+							<li key={`${category}${index}`}>
+								{category.name}
+							</li>
+						);
+					})}
+				</ul>
 
 				
 			  
